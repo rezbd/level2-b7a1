3b6f5e0a69ff7f1a6628836aeb6ae683a0021da6
Rezwanur Rakib Chy <rezwan6@gmail.com>
2026-05-26 17:06:41 +0600

    solution 4 is done

--- solutions.ts
+++ solutions.ts
@@ -26,3 +26,8 @@ function checkType(data: StringOrNumber) {
         return "Number";
     }
 }
+
+// solution 4
+function getProperty<T, K extends keyof T>(obj:T, key:K): T[K]{
+    return obj[key];
+}
